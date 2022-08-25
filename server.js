3cd96d7df09d23981f9a072265c10203a68b7fbb
Skaelet <danielandres.98.da@gmail.com>
2022-08-25 20:51:49 -0300

    Limpiando código y corrigiendo estilos.

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ const io = require("socket.io")(httpServer);
 
 httpServer.listen(PORT, () => console.log("SERVER ON"));
 
-/* const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
-}); */
-
-/* server.on('error', (error) => console.log(`Error en servidor ${error}`)); */
-
 app.use('/public', express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -37,60 +31,19 @@ io.on('connection', (socket) => {
     await listMensajes.save(messaje);
     io.sockets.emit('messaje', messaje);
   })
-})
+});
 
 
 app.get('/', async(req, res) => {
   const productsList = await contenedor.getAll();
-  const messajesList = await listMensajes.getAll()
+  const messajesList = await listMensajes.getAll();
   const productsExist = productsList.length != 0;
   const messajesExist = messajesList.length != 0;
   res.render('pages/home', 
-  { 
-    title: 'Listado de productos', 
+  {
     products:  productsList, 
     productsExist: productsExist,
     messajesList: messajesList,
     messajesExist: messajesExist
   });
-})
-
-
-/* app.get('/productos', async(req, res) => {
-  const productsList = await contenedor.getAll();
-  const productsExist = productsList.length != 0;
-  res.render('pages/products', 
-  { 
-    title: 'Listado de productos', 
-    products:  productsList, 
-    productsExist: productsExist
-  });
 });
-
-app.get('/', async(req, res) => {
-  res.render('pages/form', {title: 'Carga de productos'});
-});
-
-app.get('/home', async(req, res) => {
-  const productsList = await contenedor.getAll();
-  const productsExist = productsList.length != 0;
-  res.render('pages/home', 
-  { 
-    title: 'Listado de productos', 
-    products:  productsList, 
-    productsExist: productsExist
-  });
-})
-
-app.post('/productos', async(req, res) => {
-  const { title, price, thumbnail } = req.body;
-  const newProd = {
-    title: title,
-    price: price,
-    thumbnail: thumbnail
-  }
-
-  await contenedor.save(newProd);
-
-  res.render('pages/form', {title: 'Carga de productos'});
-}) */
