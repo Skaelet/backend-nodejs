704eb7a190c0f42b2ae5999efe751a5489b10ef4
Skaelet <danielandres.98.da@gmail.com>
2022-11-17 19:40:15 -0300

    Modo cluster y fork añadidos.

--- server.js
+++ server.js
@@ -3,12 +3,28 @@ const args = parseArgs(process.argv.slice(2));
 const { startMiddlewares } = require('./middlewares/index');
 const express = require('express');
 const app = express();
-const PORT = args.p || 8080;
+const PORT = args.port || 8080;
+const MODO = args.modo || 'FORK';
 const httpServer = require("http").createServer(app);
 const { startSocketConnection } = require('./middlewares/sockets');
 const { getRoutes } = require('./routes');
+const threads = require('os').cpus().length;
+const cluster = require('cluster');
 
-httpServer.listen(PORT, () => console.log("SERVER ON"));
+if (MODO === 'FORK') {
+  httpServer.listen(PORT, () => console.log("SERVER ON"));
+} else {
+  if (cluster.isPrimary) {
+    httpServer.listen(PORT, () => console.log("SERVER ON"));
+  } else {
+    for (let i = 0; i < threads; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', (worker) => {
+      console.log(`worker ${worker.process.pid} died`);
+    })
+  }
+}
 
 startMiddlewares(app);
 startSocketConnection(httpServer);
